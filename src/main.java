8124a499264926916371c5cd7ebb60c92ae65951
Andsemandse <andr90m3@stud.kea.dk>
2022-09-09 11:26:39 +0200

    SuperGreeter

--- src/main.java
+++ src/main.java
@@ -3,4 +3,9 @@ public class main {
         SuperGreeter classobj = new SuperGreeter();
         classobj.printMessage();
     }
+    public class main {
+        public static void main(String[] args) {
+            SuperGreeter classobj = new SuperGreeter();
+            classobj.printMessage();
 }
+
